4a359c0e78c5db4fe7dcf5f4c798719cecaab8a1
Ruchir <ruchirsaxena@gmail.com>
2018-12-03 00:25:34 +0530

    Complete Setup with react and MobX

--- demo-app/src/index.js
+++ demo-app/src/index.js
@@ -3,8 +3,4 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-import { usestrict } from "mobx";
-
-usestrict(true);
-
 ReactDOM.render(<App />, document.getElementById("root"));
