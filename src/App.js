ca26916be55831797eacd6baf2c6806578fa79a5
Ruchir <ruchirsaxena@gmail.com>
2018-12-09 01:12:08 +0530

    Used React-MobX with using small bird app

--- src/App.js
+++ src/App.js
@@ -4,11 +4,33 @@ import { inject, observer } from "mobx-react";
 @inject("BirdStore")
 @observer
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.handelSubmit = this.handelSubmit.bind(this);
+  }
+
+  handelSubmit(e) {
+    e.preventDefault();
+    const bird = this.bird.value;
+    this.props.BirdStore.addBird(bird);
+    this.bird.value = "";
+  }
   render() {
     const { BirdStore } = this.props;
+    const birdsData = BirdStore.birds.map(bird => <li>{bird}</li>);
     return (
       <div>
         <h2>You have {BirdStore.birdCount} birds in store </h2>
+        <form onSubmit={this.handelSubmit}>
+          <input
+            type="text"
+            placeholder="Enter bird"
+            ref={input => (this.bird = input)}
+          />
+          <br />
+          <button>Add bird</button>
+          <ul>{birdsData}</ul>
+        </form>
       </div>
     );
   }
